dcc7356bd964b3604e1cb297f6f6b57d7683673d
Hyertime <159103175+Hyertime@users.noreply.github.com>
2024-05-14 14:42:41 +0530

    Add files via upload

---
+++ res.js
@@ -0,0 +1,7 @@
+// Get the <h1> element containing the name
+const nameElement = document.querySelector('header h1');
+
+// Add event listener for mouseover event
+nameElement.addEventListener('mouseover', () => {
+  alert('Welcome to Suraj Thakur\'s resume!');
+});
